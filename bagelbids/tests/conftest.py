56769235e604765fe23dd77da6da7df20ae5beab
Sebastian Urchs <sebastian.urchs@gmail.com>
2022-05-25 23:57:36 -0400

    use tmp path factory for test data

--- bagelbids/tests/conftest.py
+++ bagelbids/tests/conftest.py
@@ -1,9 +1,17 @@
 from pathlib import Path
+import shutil
 
 import pytest
 
 
 @pytest.fixture(scope="session")
-def bids_synthetic():
+def bids_path(tmp_path_factory):
     bids_examples = Path(__file__).absolute().parent / "data"
-    return bids_examples / "synthetic"
+    bids_tmp_path = tmp_path_factory.mktemp("bids_examples") / "data"
+    shutil.copytree(bids_examples, bids_tmp_path)
+    return bids_tmp_path
+
+
+@pytest.fixture(scope="session")
+def bids_synthetic(bids_path):
+    return bids_path / "synthetic"
--- bagelbids/tests/test_cli.py
+++ bagelbids/tests/test_cli.py
@@ -17,8 +17,3 @@ def test_bids(bids_synthetic):
 def test_fails_if_no_arguments_provided(runner):
     result = runner.invoke(bagel)
     assert result.exit_code == 2
-
-
-def test_parsing_bids_synth_gives_json(bids_synthetic, tmp_path, runner):
-    # Setup
-    result = runner.invoke(bagel)

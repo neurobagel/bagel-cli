05153e93a0eb353c364be696ed6154f285708eb3
Sebastian Urchs <sebastian.urchs@gmail.com>
2022-05-26 09:41:08 -0400

    Add new attribute schemaKey to enable jsonld serialization
    This is taken directly from the way DANDI handles it in their
    datamodels. The schemaKey attribute will be mapped to the
    jsonld property @type with a (yet to be created) context file.

--- bagelbids/models.py
+++ bagelbids/models.py
@@ -1,21 +1,25 @@
-from typing import List
-from pydantic import BaseModel
+from typing import List, Literal
+from pydantic import BaseModel, Field
 
 
 class Imaging(BaseModel):
     hasContrastType: str
+    schemaKey: Literal["Imaging"] = Field("Imaging", readOnly=True)
 
 
 class Session(BaseModel):
     id: str
     hasAcquisition: List[Imaging]
+    schemaKey: Literal["Session"] = Field("Session", readOnly=True)
 
 
 class Subject(BaseModel):
     id: str
     hasSession: List[Session] = []
+    schemaKey: Literal["Subject"] = Field("Subject", readOnly=True)
 
 
 class Dataset(BaseModel):
     id: str
     hasSamples: List[Subject]
+    schemaKey: Literal["Dataset"] = Field("Dataset", readOnly=True)

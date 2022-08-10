9d4765db568185911452cf3eb41b6d76e3f8f911
Sebastian Urchs <sebastian.urchs@gmail.com>
2022-08-10 19:03:38 -0400

    Add ability to generate context file

--- bagelbids/cli.py
+++ bagelbids/cli.py
@@ -1,9 +1,36 @@
 from pathlib import Path
+import json
 
 import click
 from bids import BIDSLayout
 
-from bagelbids.models import Dataset, Imaging, Session, Subject
+from bagelbids import models
+
+
+def generate_context():
+    # Direct copy of the dandi-schema context generation function
+    # https://github.com/dandi/dandi-schema/blob/c616d87eaae8869770df0cb5405c24afdb9db096/dandischema/metadata.py
+    import pydantic
+    
+    field_preamble = {
+        "bagel": "http://neurobagel.org/vocab"
+    }
+    fields = {}
+    for val in dir(models):
+        klass = getattr(models, val)
+        if not isinstance(klass, pydantic.main.ModelMetaclass):
+            continue
+        fields[klass.__name__] = "bagel:" + klass.__name__
+        for name, field in klass.__fields__.items():
+            if name == "schemaKey":
+                fields[name] = "@type"
+            elif name not in fields:
+                fields[name] = {"@id": "bagel:" + name}
+                
+    field_preamble.update(**fields)
+                
+    return {"@context": field_preamble}
+    
 
 
 @click.command(
@@ -40,10 +67,20 @@ def bagel(bids_dir, output_dir, level, validate):
             for bids_file in layout.get(
                 subject=subject, session=session, extension=[".nii", ".nii.gz"]
             ):
-                image_list.append(Imaging(hasContrastType=bids_file.get_entities().get("suffix")))
-            session_list.append(Session(id=session, hasAcquisition=image_list))
-        subject_list.append(Subject(id=subject, hasSession=session_list))
-    dataset = Dataset(id=str(bids_dataset_name), hasSamples=subject_list)
+                image_list.append(models.Imaging(hasContrastType=bids_file.get_entities().get("suffix")))
+            session_list.append(models.Session(identifier=session, hasAcquisition=image_list))
+        subject_list.append(models.Subject(identifier=subject, hasSession=session_list))
+    dataset = models.Dataset(identifier=str(bids_dataset_name), hasSamples=subject_list)
 
+    context = generate_context()
+    
     with open(Path(output_dir) / f"{bids_dataset_name}.json", "w") as f:
         f.write(dataset.json(indent=2))
+        
+    with open(Path(output_dir) / f"bagelbids_context.json", "w") as f:
+        f.write(json.dumps(context, indent=2))
+        
+    context.update(**dataset.dict())
+    
+    with open(Path(output_dir) / f"{bids_dataset_name}.jsonld", "w") as f:
+        f.write(json.dumps(context, indent=2))

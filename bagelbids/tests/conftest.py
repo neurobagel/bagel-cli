b296ab3d4a6debdc74ce0e6075bf2213e6c62ae0
Sebastian Urchs <sebastian.urchs@gmail.com>
2022-08-10 19:31:25 -0400

    Remove duplicate test data

--- bagelbids/tests/conftest.py
+++ bagelbids/tests/conftest.py
@@ -6,10 +6,7 @@ import pytest
 
 @pytest.fixture(scope="session")
 def bids_path(tmp_path_factory):
-    bids_examples = Path(__file__).absolute().parent / "data"
-    bids_tmp_path = tmp_path_factory.mktemp("bids_examples") / "data"
-    shutil.copytree(bids_examples, bids_tmp_path)
-    return bids_tmp_path
+    return Path(__file__).absolute().parent.parent.parent / "bids-examples"
 
 
 @pytest.fixture(scope="session")
@@ -18,8 +15,8 @@ def bids_synthetic(bids_path):
 
 
 @pytest.fixture(scope="session")
-def bids_invalid_synthetic(bids_path, bids_synthetic):
-    invalid_path = bids_path / "synthetic_invalid"
+def bids_invalid_synthetic(bids_path, bids_synthetic, tmp_path_factory):
+    invalid_path = tmp_path_factory.mktemp("tmp_bids") / "synthetic_invalid"
     # We make a copy of the valid BIDS dataset and delete a required file to make it invalid
     shutil.copytree(bids_synthetic, invalid_path)
     (invalid_path / "dataset_description.json").unlink()

6d1e3ce59ead381a23a354fe8e5868e0c25a7116
Sebastian Urchs <sebastian.urchs@gmail.com>
2022-09-23 20:24:40 -0400

    Add mappings from BIDS to NIDM
    These are taken directly from pyNIDM

---
+++ bagelbids/mappings.py
@@ -0,0 +1,13 @@
+NIDM = {
+    "anat": "nidm:Anatomical",
+    "func": "nidm:Functional",
+    "dwi": "nidm:DiffusionWeighted",
+    "bval": "nidm:b-value",
+    "bvec": "nidm:b-vector",
+    "T1w": "nidm:T1Weighted",
+    "T2w": "nidm:T2Weighted",
+    "inplaneT2": "nidm:T2Weighted",
+    "bold": "nidm:FlowWeighted",
+    "dti": "nidm:DiffusionTensor",
+    "asl": "nidm:ArterialSpinLabeling",
+}

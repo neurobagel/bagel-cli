248eeeff87ffd16beed458ad1ead2a54de598631
Sebastian Urchs <sebastian.urchs@gmail.com>
2023-02-06 11:47:47 -0500

    Call things "phenotypic" in the docs

--- bagelbids/cli.py
+++ bagelbids/cli.py
@@ -7,10 +7,10 @@ bagel = typer.Typer()
 
 @bagel.command()
 def pheno(
-        pheno: Path = typer.Option(..., help="The path to a demographic .tsv file.",
+        pheno: Path = typer.Option(..., help="The path to a phenotypic .tsv file.",
                                    exists=True, file_okay=True, dir_okay=False),
         dictionary: Path = typer.Option(..., help="The path to the .json data dictionary "
-                                                  "corresponding to the demographic .tsv file.",
+                                                  "corresponding to the phenotypic .tsv file.",
                                         exists=True, file_okay=True, dir_okay=False),
         output: Path = typer.Option(..., help="The directory where outputs should be created",
                                     exists=True, file_okay=False, dir_okay=True)

23cab5fd7c0a934ab2279475816c3e9e5b2eb4d4
Sebastian Urchs <surchs@users.noreply.github.com>
2023-02-06 11:49:55 -0500

    Switching to typer (#47)
    * Switch to typer
    * Change base command name to `bagel`

--- bagelbids/cli.py
+++ bagelbids/cli.py
@@ -1,75 +1,26 @@
 from pathlib import Path
-from typing import Dict
-import warnings
 
-import click
-from bids import BIDSLayout
+import typer
 
-from bagelbids import models, mappings
+bagel = typer.Typer()
 
 
-def map_term_to_namespace(term: str, namespace: Dict):
+@bagel.command()
+def pheno(
+        pheno: Path = typer.Option(..., help="The path to a phenotypic .tsv file.",
+                                   exists=True, file_okay=True, dir_okay=False),
+        dictionary: Path = typer.Option(..., help="The path to the .json data dictionary "
+                                                  "corresponding to the phenotypic .tsv file.",
+                                        exists=True, file_okay=True, dir_okay=False),
+        output: Path = typer.Option(..., help="The directory where outputs should be created",
+                                    exists=True, file_okay=False, dir_okay=True)
+):
     """
-    Returns the mapped namespace term if it exists, or False otherwise"""
-    return namespace.get(term, False)
+    Process a tabular phenotypic file (.tsv) that has been successfully annotated
+    with the Neurobagel annotation tool. The annotations are expected to be stored
+    in a data dictionary (.json).
 
-
-@click.command(
-    help="The bagelbids parser\n\n"
-    ""
-    "Helps you to parse a BIDS dataset into a jsonld summary file that is ready for the "
-    "neurobagel graph store."
-)
-@click.option(
-    "--bids_dir",
-    type=click.Path(file_okay=False, dir_okay=True, exists=True),
-    help="The path to a valid BIDS directory on your system that you want to parse",
-    required=True,
-)
-@click.option(
-    "--output_dir",
-    type=click.Path(file_okay=False, dir_okay=True),
-    help="The path to an output directory where you want the parsed summary file to be stored.",
-    required=True,
-)
-@click.option("--analysis_level", "level", type=click.Choice(["group"]))
-@click.option("--validate/--skip-validate", default=True)
-def bagel(bids_dir, output_dir, level, validate):
-    # TODO setup logger
-    file_name = Path(bids_dir).name
-    layout = BIDSLayout(bids_dir, validate=validate)
-    if layout.description is None and not validate:
-        warnings.warn(
-            "The BIDS dataset_description.json file is missing.\n"
-            "We can therefore not read the BIDS dataset name.\n"
-            f"We will use the name of the BIDS directory as the dataset name: {file_name}."
-        )
-        bids_dataset_name = file_name
-    else:
-        bids_dataset_name = layout.get_dataset_description().get("Name", "Unnamed Dataset")
-
-    subject_list = []
-    for subject in layout.get_subjects():
-        session_list = []
-        for session in layout.get_sessions(subject=subject):
-            image_list = []
-            for bids_file in layout.get(
-                subject=subject, session=session, extension=[".nii", ".nii.gz"]
-            ):
-                # If the suffix of a BIDS file is not recognized, then ignore
-                mapped_term = map_term_to_namespace(
-                    bids_file.get_entities().get("suffix"), namespace=mappings.NIDM
-                )
-                if mapped_term:
-                    image_list.append(
-                        models.Acquisition(
-                            hasContrastType=models.Image(identifier=mapped_term),
-                        )
-                    )
-            session_list.append(models.Session(label=session, hasAcquisition=image_list))
-        # pyBIDS strips the "sub-" prefix, but we want to add it back
-        subject_list.append(models.Subject(label=f"sub-{subject}", hasSession=session_list))
-    dataset = models.Dataset(label=str(bids_dataset_name), hasSamples=subject_list)
-
-    with open(Path(output_dir) / f"{file_name}.json", "w") as f:
-        f.write(dataset.json(indent=2))
+    This tool will create a valid, subject-level instance of the Neurobagel
+    graph datamodel for the provided phenotypic file in the .jsonld format.
+    You can upload this .jsonld file to the Neurobagel graph.
+    """

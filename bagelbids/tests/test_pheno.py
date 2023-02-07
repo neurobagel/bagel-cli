a85c3630dd52aae18b5eaecdfab505692fd1f850
Sebastian Urchs <sebastian.urchs@gmail.com>
2023-02-07 12:57:39 -0500

    Remove superfluous validation functions
    Co-authored-by: Alyssa Dai <alyssa.ydai@gmail.com>

--- bagelbids/tests/test_pheno.py
+++ bagelbids/tests/test_pheno.py
@@ -3,28 +3,7 @@ import json
 import pytest
 import pandas as pd
 
-from bagelbids.cli import is_valid_data_dictionary, are_inputs_compatible
-
-
-@pytest.mark.parametrize("input_p,is_valid", [
-    ("example1.json", True),
-    ("example2.json", True),
-    ("example3.json", False),
-    ("example4.json", True),
-    ("example5.json", True),
-    ("example_invalid.json", False)
-])
-def test_validates_data_dictionary(test_data, input_p, is_valid):
-    """
-    Detects whether the provided data dictionary is valid under the schema.
-    Because the data dictionary schema is valid for BIDS data dictionaries without Neurobagel annotations,
-    this also detects whether the data dictionary has the required Neurobagel annotations.
-    """
-    with open(test_data / input_p, "r") as f:
-        data_dict = json.load(f)
-
-    result = is_valid_data_dictionary(data_dict)
-    assert result == is_valid
+from bagelbids.cli import are_inputs_compatible
 
 
 @pytest.mark.parametrize("example,is_valid", [

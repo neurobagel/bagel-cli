73f5e89fdcb2dd8e2f775ac5fd700d8cdfc8fa71
Alyssa Dai <alyssa.ydai@gmail.com>
2024-08-14 20:40:35 -0400

    test new input file handling errors

--- bagel/tests/test_utility.py
+++ bagel/tests/test_utility.py
@@ -4,11 +4,13 @@ from pathlib import Path
 
 import pandas as pd
 import pytest
+import typer
 from bids import BIDSLayout
 
 import bagel.bids_utils as butil
 import bagel.pheno_utils as putil
 from bagel import mappings
+from bagel.utility import load_json
 
 
 @pytest.fixture
@@ -474,3 +476,30 @@ def test_get_session_path_when_session_missing(bids_sub_id):
     assert session_path.endswith(f"sub-{bids_sub_id}")
     assert Path(session_path).is_absolute()
     assert Path(session_path).is_dir()
+
+
+@pytest.mark.parametrize(
+    "unreadable_json,expected_message",
+    [
+        ("example_iso88591.json", "Failed to decode the input file"),
+        ("example_invalid_json.json", "not valid JSON"),
+    ],
+)
+def test_failed_json_reading_raises_informative_error(
+    test_data, unreadable_json, expected_message, capsys
+):
+    """Test that when there is an issue reading an input JSON file, the CLI exits with an informative error message."""
+    with pytest.raises(typer.Exit):
+        load_json(test_data / unreadable_json)
+    captured = capsys.readouterr()
+
+    assert expected_message in captured.err
+
+
+def test_unsupported_tsv_encoding_raises_informative_error(test_data, capsys):
+    """Test that given an input phenotypic TSV with an unsupported encoding, the CLI exits with an informative error message."""
+    with pytest.raises(typer.Exit):
+        putil.load_pheno(test_data / "example_iso88591.tsv")
+    captured = capsys.readouterr()
+
+    assert "Failed to decode the input file" in captured.err

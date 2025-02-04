b5686a0b926e1eb333dad1dec9b1873a4320b058
Alyssa Dai <alyssa.ydai@gmail.com>
2025-02-04 00:59:57 -0500

    simplify mount point in generate_neurobagel_example_jsonlds.sh

--- helper_scripts/generate_neurobagel_example_jsonlds.sh
+++ helper_scripts/generate_neurobagel_example_jsonlds.sh
@@ -12,7 +12,7 @@ cd tests
 data_dir=neurobagel_examples/data-upload
 
 # Phenotypic data only JSONLD
-docker run --rm --volume=$PWD:/data/neurobagel/bagel-cli -w /data/neurobagel/bagel-cli bagel pheno \
+docker run --rm --volume=$PWD:/data -w /data bagel pheno \
     --pheno "${data_dir}/example_synthetic.tsv" \
     --dictionary "${data_dir}/example_synthetic.json" \
     --name "BIDS synthetic" \
@@ -20,21 +20,21 @@ docker run --rm --volume=$PWD:/data/neurobagel/bagel-cli -w /data/neurobagel/bag
     --overwrite
 
 # Phenotypic & BIDS data JSONLD
-docker run --rm --volume=$PWD:/data/neurobagel/bagel-cli -w /data/neurobagel/bagel-cli bagel bids \
+docker run --rm --volume=$PWD:/data -w /data bagel bids \
     --jsonld-path ${data_dir}/example_synthetic.jsonld \
     --bids-dir bids-examples/synthetic \
     --output ${data_dir}/pheno-bids-output/example_synthetic_pheno-bids.jsonld \
     --overwrite
 
 # Phenotypic & derivatives data JSONLD
-docker run --rm --volume=$PWD:/data/neurobagel/bagel-cli -w /data/neurobagel/bagel-cli bagel derivatives \
+docker run --rm --volume=$PWD:/data -w /data bagel derivatives \
     --tabular ${data_dir}/nipoppy_proc_status_synthetic.tsv \
     --jsonld-path ${data_dir}/example_synthetic.jsonld \
     --output "${data_dir}/pheno-derivatives-output/example_synthetic_pheno-derivatives.jsonld" \
     --overwrite
 
 # Phenotypic, BIDS, and derivatives data JSONLD
-docker run --rm --volume=$PWD:/data/neurobagel/bagel-cli -w /data/neurobagel/bagel-cli bagel derivatives \
+docker run --rm --volume=$PWD:/data -w /data bagel derivatives \
     --tabular ${data_dir}/nipoppy_proc_status_synthetic.tsv \
     --jsonld-path "${data_dir}/pheno-bids-output/example_synthetic_pheno-bids.jsonld" \
     --output "${data_dir}/pheno-bids-derivatives-output/example_synthetic_pheno-bids-derivatives.jsonld" \

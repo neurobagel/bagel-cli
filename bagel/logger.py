df388c254854a1364cd5c792906fafc128d0fab4
Alyssa Dai <alyssa.ydai@gmail.com>
2025-03-31 16:38:10 -0400

    add ability to log errors with or without a traceback

--- bagel/logger.py
+++ bagel/logger.py
@@ -1,4 +1,6 @@
 import logging
+import sys
+import traceback
 
 from rich.logging import RichHandler
 
@@ -12,7 +14,7 @@ def get_logger(level: int = logging.INFO) -> logging.Logger:
     """Create a logger with the specified logging level."""
 
     logger = logging.getLogger("test")
-    handler = RichHandler(omit_repeated_times=False)
+    handler = RichHandler(omit_repeated_times=False, show_path=False)
     formatter = logging.Formatter(fmt=LOG_FMT, datefmt=DATETIME_FMT)
     logger.setLevel(level)
     handler.setLevel(level)
@@ -23,6 +25,17 @@ def get_logger(level: int = logging.INFO) -> logging.Logger:
     logger.propagate = False
 
     return logger
+
+
+def log_error(
+    logger: logging.Logger, message: str, show_traceback: bool = False
+):
+    """Log an exception with or without the full traceback."""
+    if show_traceback:
+        logger.error("%s\n%s", traceback.format_exc(), message)
+    else:
+        logger.error(message)
+    sys.exit(1)
 
 
 logger = get_logger()

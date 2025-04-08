adbfc4ba62dc5b24d3c0b0a4774e1a0500caff14
Alyssa Dai <alyssa.ydai@gmail.com>
2025-04-08 13:25:05 -0400

    do not include traceback in error, and configure single logger instance

--- bagel/logger.py
+++ bagel/logger.py
@@ -4,42 +4,38 @@ from typing import NoReturn
 import typer
 from rich.logging import RichHandler
 
-from bagel.config import CONFIG
-
 LOG_FMT = "%(message)s"
 DATETIME_FMT = "[%Y-%m-%d %X]"
+
+logger = logging.getLogger("bagel.logger")
 
 
 # TODO: Once we introduce CLI options for reducing or increasing verbosity,
 # we can use the "level" parameter to set the logging level
-def get_logger(level: int = logging.INFO) -> logging.Logger:
-    """Create a logger with the specified logging level."""
+def configure_logger(level: int = logging.INFO):
+    """Configure a logger with the specified logging level."""
 
-    logger = logging.getLogger(__name__)
-    handler = RichHandler(
-        omit_repeated_times=False, show_path=False, rich_tracebacks=True
-    )
-    formatter = logging.Formatter(fmt=LOG_FMT, datefmt=DATETIME_FMT)
+    # Prevent duplicate handlers
+    if not logger.handlers:
+        handler = RichHandler(
+            omit_repeated_times=False, show_path=False, rich_tracebacks=True
+        )
+        formatter = logging.Formatter(fmt=LOG_FMT, datefmt=DATETIME_FMT)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
     logger.setLevel(level)
-    handler.setLevel(level)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    for handler in logger.handlers:
+        handler.setLevel(level)
 
     # propagate should be False to avoid duplicate messages from root logger
     logger.propagate = False
-
-    return logger
 
 
 def log_error(
     logger: logging.Logger,
     message: str,
 ) -> NoReturn:
-    """Log an exception with or without the full traceback, and exit the app."""
-    # when exc_info=True, the current exception information will be fetched
-    # and included in the log after the custom message
-    logger.error(message, exc_info=CONFIG["debug"])
+    """Log an exception with an informative error message, and exit the app."""
+    logger.error(message)
     raise typer.Exit(code=1)
-
-
-logger = get_logger()

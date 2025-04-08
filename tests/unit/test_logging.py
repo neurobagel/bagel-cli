01a913f9782eec32b955ce2f015ca84543b739a2
Alyssa Dai <alyssa.ydai@gmail.com>
2025-04-08 14:31:17 -0400

    test verbosity level

--- tests/unit/test_logging.py
+++ tests/unit/test_logging.py
@@ -1,40 +1,48 @@
+import logging
+
 import pytest
 
 from bagel.cli import bagel
 
 
 @pytest.mark.parametrize(
-    "extra_flag,should_error_include_traceback",
-    [
-        ("", False),
-        ("--debug", True),
-    ],
+    "verbosity_level,expected_log_levels",
+    [("1", {logging.INFO, logging.ERROR}), ("0", {logging.ERROR})],
 )
-def test_debug_level(
+def test_verbosity_level(
     runner,
     test_data,
     tmp_path,
-    extra_flag,
-    should_error_include_traceback,
+    verbosity_level,
+    expected_log_levels,
     caplog,
-    propagate_errors,
 ):
     result = runner.invoke(
         bagel,
         [
             "pheno",
             "--pheno",
-            test_data / "example_iso88591.tsv",
+            test_data / "example1.tsv",
             "--dictionary",
-            test_data / "example_iso88591.json",
+            test_data / "example1.json",
             "--output",
             tmp_path / "pheno.jsonld",
             "--name",
             "testing dataset",
-            extra_flag,
+            "--verbosity",
+            verbosity_level,
         ],
         catch_exceptions=False,
     )
 
+    captured_log_levels = {record.levelno for record in caplog.records}
+    missing_log_levels = expected_log_levels - captured_log_levels
+    unexpected_log_levels = captured_log_levels - expected_log_levels
+
     assert result.exit_code != 0
-    assert ("Traceback" in caplog.text) == should_error_include_traceback
+    assert (
+        not missing_log_levels
+    ), f"Missing log level(s): {missing_log_levels}"
+    assert (
+        not unexpected_log_levels
+    ), f"Unexpected log level(s): {unexpected_log_levels}"

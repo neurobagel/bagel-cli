62f15b24bdf7af538b0c7e31a4cc009808afcb65
Alyssa Dai <alyssa.ydai@gmail.com>
2025-07-30 14:57:09 -0400

    remove leftover comment
    Co-authored-by: Arman Jahanpour <77515879+rmanaem@users.noreply.github.com>

--- bagel/mappings.py
+++ bagel/mappings.py
@@ -45,7 +45,6 @@ NEUROBAGEL = {
     "sex": NB.pf + ":Sex",
     "age": NB.pf + ":Age",
     "diagnosis": NB.pf + ":Diagnosis",
-    # "healthy_control": NCIT.pf + ":C94342",
     "subject_group": NB.pf + ":SubjectGroup",
     "assessment_tool": NB.pf + ":Assessment",
 }

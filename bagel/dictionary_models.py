868aee836945391adbbf0bdb57e9f207a197f018
Alyssa Dai <alyssa.ydai@gmail.com>
2025-11-20 22:52:56 -0500

    [MNT] Update all type annotations to 3.9+ style (#602)
    * update dictionary models to 3.9+ annot style
    
    * update graph data models to 3.9+ style
    
    * update type annotations in utilities
    
    * use Annotated style for class fields where extra metadata are needed

--- bagel/dictionary_models.py
+++ bagel/dictionary_models.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Literal, Union
+from typing import Annotated, Literal
 
 from pydantic import (
     AfterValidator,
@@ -8,10 +8,9 @@ from pydantic import (
     RootModel,
 )
 from pydantic_core import PydanticCustomError
-from typing_extensions import Annotated
 
 
-def validate_unique_list(values: List[str]) -> List[str]:
+def validate_unique_list(values: list[str]) -> list[str]:
     """
     Check that provided list only has unique elements.
 
@@ -33,29 +32,38 @@ def validate_unique_list(values: List[str]) -> List[str]:
 class Term(BaseModel):
     """An identifier of a controlled term with an IRI"""
 
-    termURL: str = Field(
-        ...,
-        description="An unambiguous identifier for the term, concept or entity that is referenced",
-        alias="TermURL",
-    )
-    label: str = Field(
-        ...,
-        description="A human readable label. If more than one label exists for the term, "
-        "then the preferred label should be used.",
-        alias="Label",
-    )
+    termURL: Annotated[
+        str,
+        Field(
+            ...,
+            description="An unambiguous identifier for the term, concept or entity that is referenced",
+            alias="TermURL",
+        ),
+    ]
+    label: Annotated[
+        str,
+        Field(
+            ...,
+            description="A human readable label. If more than one label exists for the term, "
+            "then the preferred label should be used.",
+            alias="Label",
+        ),
+    ]
 
 
 class Neurobagel(BaseModel):
     """The base model for a Neurobagel column annotation"""
 
-    isAbout: Term = Field(
-        ...,
-        description="The concept or controlled term that describes this column",
-        alias="IsAbout",
-    )
+    isAbout: Annotated[
+        Term,
+        Field(
+            ...,
+            description="The concept or controlled term that describes this column",
+            alias="IsAbout",
+        ),
+    ]
     missingValues: Annotated[
-        List[str],
+        list[str],
         AfterValidator(validate_unique_list),
         Field(
             [],
@@ -72,12 +80,17 @@ class Neurobagel(BaseModel):
 class IdentifierNeurobagel(BaseModel):
     """A Neurobagel annotation for an identifier column"""
 
-    isAbout: Term = Field(
-        ...,
-        description="The concept or controlled term that describes this column",
-        alias="IsAbout",
-    )
-    variableType: Literal["Identifier"] = Field(..., alias="VariableType")
+    isAbout: Annotated[
+        Term,
+        Field(
+            ...,
+            description="The concept or controlled term that describes this column",
+            alias="IsAbout",
+        ),
+    ]
+    variableType: Annotated[
+        Literal["Identifier"], Field(..., alias="VariableType")
+    ]
 
     model_config = ConfigDict(extra="forbid")
 
@@ -85,28 +98,38 @@ class IdentifierNeurobagel(BaseModel):
 class CategoricalNeurobagel(Neurobagel):
     """A Neurobagel annotation for a categorical column"""
 
-    levels: Dict[str, Term] = Field(
-        ...,
-        description="For categorical variables: "
-        "An object of values (keys) in the column and the semantic "
-        "term (URI and label) they are unambiguously mapped to.",
-        alias="Levels",
-    )
-    variableType: Literal["Categorical"] = Field(..., alias="VariableType")
+    levels: Annotated[
+        dict[str, Term],
+        Field(
+            ...,
+            description="For categorical variables: "
+            "An object of values (keys) in the column and the semantic "
+            "term (URI and label) they are unambiguously mapped to.",
+            alias="Levels",
+        ),
+    ]
+    variableType: Annotated[
+        Literal["Categorical"], Field(..., alias="VariableType")
+    ]
 
 
 class ContinuousNeurobagel(Neurobagel):
     """A Neurobagel annotation for a continuous column"""
 
-    format: Term = Field(
-        ...,
-        description="For continuous columns this field is used to describe "
-        "the format of the raw numerical values in the column. This information is used to transform "
-        "the column values into the desired format of the standardized "
-        "data element referenced in the IsAbout attribute.",
-        alias="Format",
-    )
-    variableType: Literal["Continuous"] = Field(..., alias="VariableType")
+    format: Annotated[
+        Term,
+        Field(
+            ...,
+            description="For continuous columns this field is used to describe "
+            "the format of the raw numerical values in the column. This information is used to transform "
+            "the column values into the desired format of the standardized "
+            "data element referenced in the IsAbout attribute.",
+            alias="Format",
+        ),
+    ]
+    variableType: Annotated[
+        Literal["Continuous"], Field(..., alias="VariableType")
+    ]
 
 
 class CollectionNeurobagel(Neurobagel):
@@ -115,15 +138,18 @@ class CollectionNeurobagel(Neurobagel):
     such as items from an instrument.
     """
 
-    # NOTE: Optional[Identifier] was removed as part of https://github.com/neurobagel/bagel-cli/pull/389
-    # because we couldn't tell what the Optional was doing
-    isPartOf: Term = Field(
-        ...,
-        description="If the column is a subscale or item of an assessment tool "
-        "then the assessment tool should be specified here.",
-        alias="IsPartOf",
-    )
-    variableType: Literal["Collection"] = Field(..., alias="VariableType")
+    isPartOf: Annotated[
+        Term,
+        Field(
+            ...,
+            description="If the column is a subscale or item of an assessment tool "
+            "then the assessment tool should be specified here.",
+            alias="IsPartOf",
+        ),
+    ]
+    variableType: Annotated[
+        Literal["Collection"], Field(..., alias="VariableType")
+    ]
 
 
 class Column(BaseModel):
@@ -132,44 +158,54 @@ class Column(BaseModel):
     # TODO: Revisit if we want to make description an optional field, since we don't currently use it in the graph data.
     # At the moment, the key itself is always required and the value can be an empty string "",
     # but a value of null ("Description": null) is invalid and will result in a schema validation error.
-    description: str = Field(
-        ...,
-        description="Free-form natural language description",
-        alias="Description",
-    )
-    annotations: Union[
-        CategoricalNeurobagel,
-        ContinuousNeurobagel,
-        IdentifierNeurobagel,
-        CollectionNeurobagel,
-    ] = Field(None, description="Semantic annotations", alias="Annotations")
+    description: Annotated[
+        str,
+        Field(
+            ...,
+            description="Free-form natural language description",
+            alias="Description",
+        ),
+    ]
+    annotations: Annotated[
+        CategoricalNeurobagel
+        | ContinuousNeurobagel
+        | IdentifierNeurobagel
+        | CollectionNeurobagel,
+        Field(None, description="Semantic annotations", alias="Annotations"),
+    ]
 
 
 class CategoricalColumn(Column):
     """A BIDS column annotation for a categorical column"""
 
-    levels: Dict[str, str] = Field(
-        ...,
-        description="For categorical variables: "
-        "An object of possible values (keys) "
-        "and their descriptions (values). ",
-        alias="Levels",
-    )
+    levels: Annotated[
+        dict[str, str],
+        Field(
+            ...,
+            description="For categorical variables: "
+            "An object of possible values (keys) "
+            "and their descriptions (values). ",
+            alias="Levels",
+        ),
+    ]
 
 
 class ContinuousColumn(Column):
     """A BIDS column annotation for a continuous column"""
 
-    units: str = Field(
-        ...,
-        description="Measurement units for the values in this column. "
-        "SI units in CMIXF formatting are RECOMMENDED (see Units)",
-        alias="Units",
-    )
+    units: Annotated[
+        str,
+        Field(
+            ...,
+            description="Measurement units for the values in this column. "
+            "SI units in CMIXF formatting are RECOMMENDED (see Units)",
+            alias="Units",
+        ),
+    ]
 
 
 class DataDictionary(
-    RootModel[Dict[str, Union[Column, ContinuousColumn, CategoricalColumn]]]
+    RootModel[dict[str, Column | ContinuousColumn | CategoricalColumn]]
 ):
     """A data dictionary with human and machine readable information for a tabular data file"""
 
--- bagel/models.py
+++ bagel/models.py
@@ -1,5 +1,5 @@
 import uuid
-from typing import List, Literal, Optional, Union
+from typing import Annotated, Literal
 
 from pydantic import BaseModel, ConfigDict, Field, HttpUrl
 
@@ -13,16 +13,19 @@ class Bagel(BaseModel):
     """identifier has to be a valid UUID prepended by the Neurobagel namespace
     by default, a random (uuid4) string UUID will be created"""
 
-    identifier: str = Field(
-        pattern=BAGEL_UUID_PATTERN,
-        default_factory=lambda: NB.pf + ":" + str(uuid.uuid4()),
-    )
+    identifier: Annotated[
+        str,
+        Field(
+            pattern=BAGEL_UUID_PATTERN,
+            default_factory=lambda: NB.pf + ":" + str(uuid.uuid4()),
+        ),
+    ]
 
     model_config = ConfigDict(extra="forbid")
 
 
 class ControlledTerm(BaseModel):
-    identifier: Union[str, HttpUrl]
+    identifier: str | HttpUrl
     schemaKey: str
 
 
@@ -66,25 +69,25 @@ class Session(Bagel):
 
 
 class PhenotypicSession(Session):
-    hasAge: Optional[float] = None
-    hasSex: Optional[Sex] = None
-    isSubjectGroup: Optional[SubjectGroup] = None
-    hasDiagnosis: Optional[List[Diagnosis]] = None
-    hasAssessment: Optional[List[Assessment]] = None
+    hasAge: float | None = None
+    hasSex: Sex | None = None
+    isSubjectGroup: SubjectGroup | None = None
+    hasDiagnosis: list[Diagnosis] | None = None
+    hasAssessment: list[Assessment] | None = None
     schemaKey: Literal["PhenotypicSession"] = "PhenotypicSession"
 
 
 class ImagingSession(Session):
     # NOTE: Do imaging session have to have at least one acquisition OR at least one completed pipeline to be valid?
-    hasFilePath: Optional[str] = None
-    hasAcquisition: Optional[List[Acquisition]] = None
-    hasCompletedPipeline: Optional[List[CompletedPipeline]] = None
+    hasFilePath: str | None = None
+    hasAcquisition: list[Acquisition] | None = None
+    hasCompletedPipeline: list[CompletedPipeline] | None = None
     schemaKey: Literal["ImagingSession"] = "ImagingSession"
 
 
 class Subject(Bagel):
     hasLabel: str
-    hasSession: List[Union[PhenotypicSession, ImagingSession]]
+    hasSession: list[PhenotypicSession | ImagingSession]
     schemaKey: Literal["Subject"] = "Subject"
 
 
@@ -92,6 +95,6 @@ class Dataset(Bagel):
     hasLabel: str
     # NOTE: Since Pydantic v2, URL types no longer inherit from `str`
     # (see https://docs.pydantic.dev/latest/migration/#url-and-dsn-types-in-pydanticnetworks-no-longer-inherit-from-str)
-    hasPortalURI: Optional[Union[str, HttpUrl]] = None
-    hasSamples: List[Subject]
+    hasPortalURI: str | HttpUrl | None = None
+    hasSamples: list[Subject]
     schemaKey: Literal["Dataset"] = "Dataset"
